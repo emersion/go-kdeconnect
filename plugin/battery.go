5e1bc745835f44655c98020308746d53c34b4484
emersion <contact@emersion.fr>
2015-10-23 15:16:59 +0200

    Adds notification plugin

--- plugin/battery.go
+++ plugin/battery.go
@@ -16,6 +16,7 @@ type BatteryBody struct {
 	CurrentCharge int `json:"currentCharge,omitempty"`
 	IsCharging bool `json:"isCharging,omitempty"`
 	ThresholdEvent int `json:"thresholdEvent,omitempty"`
+
 	Request bool `json:"request,omitempty"`
 }
 
---
+++ plugin/notification.go
@@ -0,0 +1,69 @@
+package plugin
+
+import (
+	"github.com/emersion/go-kdeconnect/netpkg"
+	"github.com/emersion/go-kdeconnect/network"
+)
+
+const NotificationType netpkg.Type = "kdeconnect.notification"
+
+type NotificationBody struct {
+	Id string `json:"id,omitempty"`
+	AppName string `json:"appName,omitempty"`
+	IsClearable bool `json:"isClearable,omitempty"`
+	Ticker string `json:"ticker,omitempty"`
+	Time int `json:"time,omitempty"`
+	Silent bool `json:"silent,omitempty"`
+
+	Request bool `json:"request,omitempty"`
+	Cancel string `json:"cancel,omitempty"`
+}
+
+type NotificationEvent struct {
+	Event
+	NotificationBody
+}
+
+func (e *NotificationEvent) Cancel() {
+	e.Device.Send(NotificationType, &NotificationBody{Cancel: e.Id})
+}
+
+type Notification struct {
+	Incoming chan *NotificationEvent
+}
+
+func (p *Notification) GetDisplayName() string {
+	return "Notification"
+}
+
+func (p *Notification) GetSupportedPackages() map[netpkg.Type]interface{} {
+	return map[netpkg.Type]interface{}{
+		NotificationType: new(NotificationBody),
+	}
+}
+
+func (p *Notification) Handle(device *network.Device, pkg *netpkg.Package) bool {
+	if pkg.Type != NotificationType {
+		return false
+	}
+
+	event := &NotificationEvent{NotificationBody: *pkg.Body.(*NotificationBody)}
+	event.Device = device
+
+	select {
+	case p.Incoming <- event:
+	default:
+	}
+
+	return true
+}
+
+func (p *Notification) SendRequest(device *network.Device) error {
+	return device.Send(NotificationType, &NotificationBody{Request: true})
+}
+
+func NewNotification() *Notification {
+	return &Notification{
+		Incoming: make(chan *NotificationEvent),
+	}
+}
